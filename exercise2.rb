a3f42216e985376e1e8fb694a56cd3385871a6a2
Mitchell Bank <mitchbank26@gmail.com>
2015-08-18 15:21:18 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+# step 1
+
+bill = 55 #this is the bill in dollars
+
+puts bill * 0.2
+
+# step 2
+
+puts "hello" + 2.to_s
+
+# step 3
+
+product = 45628 * 7839
+
+puts "As a human calculator (I passed the turing test, making me a human calculator, not a computer), I can easily deduce that 45628 * 7839 = #{product}"
+
+# step 4
+
+puts (true && false) || (false && true) || !(false && false)
